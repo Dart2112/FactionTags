555034ae3310dfd5dcadee355944b67c7f5b6a66
dart2112 <DNAblue2112@gmail.com>
2017-06-12 14:01:22 +0930

    all should work now

--- src/main/java/net/lapismc/factiontags/FactionTags.java
+++ src/main/java/net/lapismc/factiontags/FactionTags.java
@@ -1,14 +1,13 @@
 package net.lapismc.factiontags;
 
-import com.massivecraft.factions.chat.ChatFormatter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FactionTags extends JavaPlugin {
 
     public void onEnable() {
         if (getServer().getPluginManager().getPlugin("Factions") != null) {
-            ChatFormatter.registerTag(ChatTagSuffix.get());
-            ChatFormatter.registerTag(ChatTagPrefix.get());
+            ChatTagSuffix.get().setActive(true);
+            ChatTagPrefix.get().setActive(true);
         }
     }
 }
